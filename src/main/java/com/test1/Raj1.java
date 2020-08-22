2e467093e62d3ca079e29dbab6efca79abca46e9
Rajendra <gholveraj9175@gmail.com>
2020-08-22 20:26:39 +0545

    hi hello a am raj

---
+++ src/main/java/com/test1/Raj1.java
@@ -0,0 +1,10 @@
+package com.test1;
+
+public class Raj1 {
+	
+	public void test1()
+	{
+		System.out.println("This is test one method");
+	}
+
+}
---
+++ src/test/java/com/test2/raj2.java
@@ -0,0 +1,10 @@
+package com.test2;
+
+public class raj2 {
+	
+	public void set()
+	{
+		System.out.println("this is second method");
+	}
+
+}
